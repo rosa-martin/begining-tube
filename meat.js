a980e7d4738f8ed161c7b891a797026689b88344
rosa-martin <matkoro@gmail.com>
2023-08-18 21:17:23 +0200

    FUCKING FINNALY, youtu.be shortened urls are now handeled properly, extension finally works :)

--- meat.js
+++ meat.js
@@ -1,21 +1,29 @@
-window.onload = function setToZero(){
+function setToZero(){
 
     let url = window.location.href;
-    const reg = /&t=\d+s/;
+    const reg1 = /&t=\d+s/; //youtube.com/watch...
+    const reg2 = /\\?t=\d+/; //youtu.be/...
     
-    if(url.includes("&t=") && !url.includes("&t=0s")){
+    if((url.includes("&t=") && !url.includes("&t=0s")) || (url.includes("?t=") && !url.includes("?t=0"))){
         let choice = confirm("Start the video at 0:00?");
 
         if(choice){
-            let newUrl = url.replace(reg, "&t=0s");
-    
+            let newUrl;
+            if(url.includes("&t=") && !url.includes("&t=0s")){
+                newUrl = url.replace(reg1, "&t=0s");
+            } else if (url.includes("?t=") && !url.includes("?t=0")) {
+                newUrl = url.replace(reg2, "?t=0");
+            }
+            
             window.location.href = newUrl;
-            return;
         }
         
     }
     
 }
+
+window.onload = setToZero;
+//window.addEventListener("hashchange", setToZero);
 
 //function addLocationObserver(callback){
 //    let conf = { attributes: false, childList: true, subtree: false };
